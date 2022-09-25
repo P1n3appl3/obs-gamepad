2e629a93aef9f0ce5749c7db423248afc3ef7470
Joseph Ryan <josephryan3.14@gmail.com>
2022-09-25 15:06:05 -0700

    Improve install script

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 set -ex
 
-plugin_dir="$HOME"/.config/obs-studio/plugins/gamepad/
-mkdir -p "$plugin_dir"
-cargo build --release
+plugin_dir="$HOME"/.config/obs-studio/plugins/gamepad
+mkdir -p "$plugin_dir"/bin/64bit
 src=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-ln -fs "$src"/target/release/libgamepad.so "$plugin_dir"/bin/64bit/libgamepad.so
-ln -fs "$src"/example.toml "$plugin_dir"/example.toml
+cd "$src"
+cargo build --release
+cp -f "$src"/target/release/libgamepad.so "$plugin_dir"/bin/64bit
+cp -f "$src"/example.toml "$plugin_dir"
